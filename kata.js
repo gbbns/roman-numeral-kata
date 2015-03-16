a00ffda87a57c8b0e5afeb68e378ddbb785a3021
Chris Gibbons <cashiernumbersixplease@gmail.com>
2015-03-16 21:03:45 +0000

    Added conversion chart;

---
+++ kata.js
@@ -0,0 +1,20 @@
+// JS conversion of arabic to Roman Numerals
+//
+// Taken from http://en.wikipedia.org/wiki/Roman_numerals
+//
+// I : 1
+// V : 5
+// X : 10
+// L : 50
+// C : 100
+// D : 500
+// M : 1,000
+
+// Also we need to know:
+//
+// IV : 4
+// IX : 9
+// XL : 40
+// XC : 90
+// CD : 400
+// CM : 900
