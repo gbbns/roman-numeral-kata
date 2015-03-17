783df036612c3b05689a6a98ad34d2db2171b511
Chris Gibbons <cashiernumbersixplease@gmail.com>
2015-03-17 09:07:58 +0000

    Testing separation of numbers into individual arrays and simplifying operations incase something was causing error.

--- kata.js
+++ kata.js
@@ -25,32 +25,37 @@ function arabicToRoman(num){
 
   // Array of roman numerals and corresponding arabic
   //var numeralArray = ['1','I','5','V','10','X','50','L','100','C','500','D','1000','M'];
-  var numeralArray = ['1000','M','900','CM','500','D','400','CD','100','C','90','XC','50','L','40','XL','10','X','9','IX','5','V','4','IV','1','I'];
+  var numeralArray = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  var arabicArray = ['1000','900','500','400','100','90','50','40','10','9','5','4','1'];
+
   // Grab number to convert
   var toConvert = num;
 
   // Setup a variable to add numerals to
   var romanDate = '';
 
+  // Check year is valid
   if (toConvert >= 1 && toConvert <= 3999){
-    for (var i = 0; i <= numeralArray.length; i++) {
+    for (var i = 0; i <= arabicArray.length; i++) {
 
       console.log('Value of i: ' + i);
       console.log('Initial value:' + toConvert);
       console.log('Current array item: ' + numeralArray[i]);
 
-      if (toConvert >= numeralArray[i]) {
+      if (toConvert >= arabicArray[i]) {
           console.log('Array item:' + numeralArray[i + 1]);
 
           // Add one to array index to get numeral
-          romanDate += numeralArray[i + 1];
+          romanDate = romanDate + numeralArray[i];
           console.log('Date: ' + romanDate);
 
           // As per paper workouts, need to remove last pass from original date.
-          toConvert = toConvert - numeralArray[i];
+          toConvert = toConvert - arabicArray[i];
 
           // Console log results for testing.
           console.log('Updated number:' + toConvert);
+
+          i = 0;
       }
     }
 
