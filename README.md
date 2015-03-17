20f60ff330c1703a631143a2c478fd91354f1c34
Chris Gibbons <cashiernumbersixplease@gmail.com>
2015-03-17 22:14:20 +0000

    Moved comments to readme; Added function closure and “use strict” for tighter syntax

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 #Roman Numeral Kata
 
 JavaScript Kata to convert an arabic number to [Roman numerals](http://en.wikipedia.org/wiki/Roman_numerals)
+
+
+JS conversion of arabic to Roman Numerals
+
+// I : 1
+// V : 5
+// X : 10
+// L : 50
+// C : 100
+// D : 500
+// M : 1,000
+
+// Also we need to know:
+//
+// IV : 4
+// IX : 9
+// XL : 40
+// XC : 90
+// CD : 400
+// CM : 900
+
+// Rule: Only convert between 1 & 3999
--- kata.js
+++ kata.js
@@ -1,67 +1,49 @@
-// JS conversion of arabic to Roman Numerals
-//
-// Taken from http://en.wikipedia.org/wiki/Roman_numerals
-//
-// I : 1
-// V : 5
-// X : 10
-// L : 50
-// C : 100
-// D : 500
-// M : 1,000
+(function(){
 
-// Also we need to know:
-//
-// IV : 4
-// IX : 9
-// XL : 40
-// XC : 90
-// CD : 400
-// CM : 900
+  "use strict";
 
-// Rule: Only convert between 1 & 3999
+  function arabicToRoman(num){
 
-function arabicToRoman(num){
+    // Array of roman numerals and corresponding arabic
+    var numeralArray = ['1000','M','900','CM','500','D','400','CD','100','C','90','XC','50','L','40','XL','10','X','9','IX','5','V','4','IV','1','I'];
 
-  // Array of roman numerals and corresponding arabic
-  // Todo - Switch to associative array
-  var numeralArray = ['1000','M','900','CM','500','D','400','CD','100','C','90','XC','50','L','40','XL','10','X','9','IX','5','V','4','IV','1','I'];
+    // Grab number to convert
+    var toConvert = num;
 
-  // Grab number to convert
-  var toConvert = num;
+    // Setup a variable to add numerals to
+    var romanDate = '';
 
-  // Setup a variable to add numerals to
-  var romanDate = '';
+    // Check year is valid
+    if (toConvert >= 1 && toConvert <= 3999){
 
-  // Check year is valid
-  if (toConvert >= 1 && toConvert <= 3999){
+      for (var i = 0; i <= numeralArray.length; i++) {
 
-    for (var i = 0; i <= numeralArray.length; i++) {
+        console.log('Value of i: ' + i);
+        console.log('Initial value:' + toConvert);
+        console.log('Current array item: ' + numeralArray[i]);
 
-      console.log('Value of i: ' + i);
-      console.log('Initial value:' + toConvert);
-      console.log('Current array item: ' + numeralArray[i]);
+        while (toConvert >= numeralArray[i]) {
+            console.log('Array item:' + numeralArray[i + 1]);
 
-      while (toConvert >= numeralArray[i]) {
-          console.log('Array item:' + numeralArray[i + 1]);
+            // Add one to array index to get numeral
+            romanDate += numeralArray[i + 1];
+            console.log('Date: ' + romanDate);
 
-          // Add one to array index to get numeral
-          romanDate += numeralArray[i + 1];
-          console.log('Date: ' + romanDate);
+            // As per paper workouts, need to remove last pass from original date.
+            toConvert -= numeralArray[i];
 
-          // As per paper workouts, need to remove last pass from original date.
-          toConvert -= numeralArray[i];
-
-          // Console log results for testing.
-          console.log('Updated number:' + toConvert);
+            // Console log results for testing.
+            console.log('Updated number:' + toConvert);
+        }
       }
+    } else {
+      return 'Please enter a value between 1 and 3999'
     }
-  } else {
-    return 'Please enter a value between 1 and 3999'
+    return romanDate;
   }
-  return toConvert;
-}
 
-console.log(arabicToRoman(2015));
 
-console.log(arabicToRoman(1999));
+  console.log('2015: ' + arabicToRoman(2015));
+  console.log('1999: ' + arabicToRoman(1999));
+
+})();
