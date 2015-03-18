bce800e777bd38cc10be7b5b6e26098ed12c1e8b
Chris Gibbons <cashiernumbersixplease@gmail.com>
2015-03-18 14:14:08 +0000

    updated array structure of arabic/numeral data to allow for easier reuse; Changed way DOM elements are injected. Added check on ‘toConvert’ so when it’s 0 the while loop doesn’t run;

--- scripts/kata.js
+++ scripts/kata.js
@@ -17,16 +17,30 @@
   }, false);
 
   function updateForm(data){
-    form.innerHTML = form.innerHTML + '<p>New date: ' + data +'</p>';
+    form.insertAdjacentHTML("beforeend", '<p>New date: ' + data +'</p>');
   }
 
   function arabicToRoman(num){
 
     // Array of roman numerals and corresponding arabic
-    var numeralArray = ['1000','M','900','CM','500','D','400','CD','100','C','90','XC','50','L','40','XL','10','X','9','IX','5','V','4','IV','1','I'];
+    var numeralArray = [
+      { arabic:1000, numeral:'M'},
+      { arabic:900, numeral:'CM'},
+      { arabic:500, numeral:'D'},
+      { arabic:400, numeral:'CD'},
+      { arabic:100, numeral:'C'},
+      { arabic:90, numeral:'XC'},
+      { arabic:50, numeral:'L'},
+      { arabic:40, numeral:'XL'},
+      { arabic:10, numeral:'X'},
+      { arabic:9, numeral:'IX'},
+      { arabic:5, numeral:'V'},
+      { arabic:4, numeral:'IV'},
+      { arabic:1, numeral:'I'}
+      ];
 
     // Grab number to convert
-    var toConvert = num;
+    var toConvert = parseInt(num);
 
     // Setup a variable to add numerals to
     var romanDate = '';
@@ -36,28 +50,30 @@
 
       for (var i = 0; i <= numeralArray.length; i++) {
 
+        var numeralArrayItem = numeralArray[i];
+
         console.log('Value of i: ' + i);
         console.log('Initial value:' + toConvert);
-        console.log('Current array item: ' + numeralArray[i]);
 
-        while (toConvert >= numeralArray[i]) {
-            console.log('Array item:' + numeralArray[i + 1]);
+        while (toConvert > 0 && toConvert >= numeralArrayItem.arabic) {
+          console.log('Current array key: ' + numeralArrayItem.arabic);
+          console.log('Current array value:' + numeralArrayItem.numeral);
 
-            // Add one to array index to get numeral
-            romanDate += numeralArray[i + 1];
-            console.log('Date: ' + romanDate);
+          romanDate += numeralArrayItem.numeral;
+          console.log('Date: ' + romanDate);
 
-            // As per paper workouts, need to remove last pass from original date.
-            toConvert -= numeralArray[i];
+          // As per paper workouts, need to remove last pass from original date.
+          toConvert -= numeralArrayItem.arabic;
 
-            // Console log results for testing.
-            console.log('Updated number:' + toConvert);
+          // Console log results for testing.
+          console.log('Updated number:' + toConvert);
         }
       }
+      return romanDate;
+
     } else {
       return 'Please enter a value between 1 and 3999'
     }
-    return romanDate;
   }
 
   //arabicToRoman(2015);
